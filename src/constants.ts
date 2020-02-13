a05cfdb52a2f8230c5915e71dced52457d0ecfeb
George Czabania <1140167+stayradiated@users.noreply.github.com>
2020-02-13 15:03:13 +1300

    chore: update dependencies

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,6 @@ export const SERVICE_ACCOUNT_EMAIL = env.get('SERVICE_ACCOUNT_EMAIL').asString()
 export const SERVICE_URL = env.get('SERVICE_URL').asUrlString()
 
 export const ACK_DEADLINE_SECONDS = env
-  .get('ACK_DEADLINE_SECONDS', '60')
+  .get('ACK_DEADLINE_SECONDS')
+  .default('60')
   .asInt()
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,7 +5,7 @@ import { createService } from './index'
 test('should create a service', (t) => {
   const testService = createService({
     serviceName: 'test-service',
-    serviceUrl: 'https://test-service.app'
+    serviceUrl: 'https://test-service.app',
   })
 
   t.truthy(testService)
